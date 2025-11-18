05736ba34f6d07501de5d804fbb7a68fa375b19a
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-11-18 13:48:48 +0500

    Task4

---
+++ Tyuiu.SafronovVE.Sprint6.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.SafronovVE.Sprint6.Task4.V6.Lib
+{
+    public class DataService : ISprint6Task4V6
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] mas = new double[len];
+            int index = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (4 * x - 0.5 == 0)
+                {
+                    mas[index++] = 0;
+                }
+                else
+                {
+                    mas[index++] = Math.Round(3 * Math.Cos(x) / (4 * x - 0.5) + Math.Sin(x) - 5 * x - 2, 2);
+                }
+            }
+            return mas;
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint6.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SafronovVE.Sprint6.Task4.V6.Lib;
+
+namespace Tyuiu.SafronovVE.Sprint6.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMass()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] waitArray = new double[len];
+
+            waitArray[0] = 23.92;
+            waitArray[1] = 18.88;
+            waitArray[2] = 13.1;
+            waitArray[3] = 7.24;
+            waitArray[4] = 1.8;
+            waitArray[5] = -8;
+            waitArray[6] = -5.7;
+            waitArray[7] = -11.26;
+            waitArray[8] = -17.12;
+            waitArray[9] = -22.88;
+            waitArray[10] = -27.92;
+
+            var resArray = ds.GetMassFunction(startValue, stopValue);
+
+            CollectionAssert.AreEqual(resArray, waitArray);
+
+        }
+    }
+}
