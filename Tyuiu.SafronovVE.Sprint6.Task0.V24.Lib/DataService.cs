8b2e3b79ff9afb4e324a0f2a44fe97cec173e9e9
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-11-29 10:46:01 +0500

    Task0

--- Tyuiu.SafronovVE.Sprint6.Task0.V24.Lib/DataService.cs
+++ Tyuiu.SafronovVE.Sprint6.Task0.V24.Lib/DataService.cs
@@ -8,6 +8,7 @@ namespace Tyuiu.SafronovVE.Sprint6.Task0.V24.Lib
             double y = Math.Pow(x - 1, 3 * x + 1);
             double res = Math.Round(y, 3);
             return res;
+
         }
     }
 }
